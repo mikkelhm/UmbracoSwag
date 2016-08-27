d31e73140ab0db431f5fef734ca04796fcdca00d
mikkelhm <mikkel@umbraco.com>
2016-08-27 23:30:58 +0200

    adding extension

---
+++ UmbracoSwag.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace UmbracoSwag.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FallbackTo(this string original, string fallback)
+        {
+            if (string.IsNullOrEmpty(original))
+                return fallback;
+            return original;
+        }
+    }
+}
