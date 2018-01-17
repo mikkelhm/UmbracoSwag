c6df6bbe3649e2071480873191e0762f5e6694bd
Mikkel Holck Madsen <mikkel@h-madsen.dk>
2018-01-17 23:31:55 +0100

    making it break

--- UmbracoSwag.Core/Extensions/StringExtensions.cs
+++ UmbracoSwag.Core/Extensions/StringExtensions.cs
@@ -1,12 +1,13 @@
 ﻿namespace UmbracoSwag.Core.Extensions
 {
     public static class StringExtensions
-    {
+    {2345
         public static string FallbackTo(this string original, string fallback)
-        {
+        {faasdf
             if (string.IsNullOrEmpty(original))
                 return fallback;
             return original;
-        }
+        }52345
     }
 }
+q323
